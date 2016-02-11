c5b02743f2ed33db27f0a0be410dbf500c785580
tjinjin <tjinjinprogram@gmail.com>
2016-02-11 21:02:40 +0900

    First commit

---
+++ lib/rake/stretcher/chef.rb
@@ -0,0 +1,9 @@
+require "rake/stretcher/chef/version"
+
+module Rake
+  module Stretcher
+    module Chef
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rake/stretcher/chef/version.rb
@@ -0,0 +1,7 @@
+module Rake
+  module Stretcher
+    module Chef
+      VERSION = "0.1.0"
+    end
+  end
+end
